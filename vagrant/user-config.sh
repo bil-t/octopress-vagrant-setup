6929c539692c5283f11f6f3e40e9d7d9b2016706
bil-t <bil-t@users.noreply.github.com>
2015-04-28 00:58:04 +0200

    better provisioning of rbenv, ruby and octopress
    	install rbenv, ruby and octopress only once
    	merge octopress project with parent project
    	set ruby version globaly

--- vagrant/user-config.sh
+++ vagrant/user-config.sh
@@ -1,33 +1,50 @@
 #!/bin/bash -x
+
+git config --global user.name "git.user"
+git config --global user.email "git.user@github.com"
+
 cd
 pwd
-#ruby installation
-git clone git://github.com/sstephenson/rbenv.git .rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+#rbenv installation
+if [ ! -d "$HOME/.rbenv" ]; then
+	git clone git://github.com/sstephenson/rbenv.git .rbenv
+	echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+	echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+	git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build	
+fi
+
 export PATH="~/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
-rbenv install 1.9.3-p0
-rbenv local 1.9.3-p0
-rbenv rehash
-ruby --version
 
-#Setup Octopress
+#ruby installation
+if [ ! -d "$HOME/.rbenv/versions/1.9.3-p0" ]; then
+	rbenv install 1.9.3-p0
+	rbenv global 1.9.3-p0
+	gem install bundler
+	rbenv rehash
+	ruby --version
+fi
+
+#switch to synced folder
 cd /vagrant
-rbenv local 1.9.3-p0
-git clone git://github.com/imathis/octopress.git octopress
-cd octopress
-#bind server to all inetrfaces
-sed -i.bak '/^server_port/a server_host     = "0.0.0.0"   # server bind address for preview server' Rakefile
-sed -i.bak 's/rackup --port #{server_port}/rackup --host #{server_host} --port #{server_port}/g' Rakefile
-#install dependencies
-gem install bundler
-rbenv rehash
-bundle install
-#install default theme
-rake install
-#install heroku
-gem install heroku
-rbenv rehash
 
+#clone octopress and merge it with the parent git project
+if [ ! -d "/vagrant/octopress" ]; then
+	git clone git://github.com/imathis/octopress.git octopress
+	rm -Rf octopress/.git
+	shopt -s dotglob
+	mv octopress/* .
+	#git config for vagrant files
+	echo '.vagrant' >> .gitignore
+	echo '*.sh text eol=lf' >> .gitattributes
+
+	#bind server to all inetrfaces
+	sed -i.bak '/^server_port/a server_host     = "0.0.0.0"   # server bind address for preview server' Rakefile
+	sed -i.bak 's/rackup --port #{server_port}/rackup --host #{server_host} --port #{server_port}/g' Rakefile
+
+	#install dependencies
+	bundle install
+	#install default theme
+	rake install
+fi
